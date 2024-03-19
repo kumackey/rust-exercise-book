237e26c2701c1222011636e4538c8c7301c4054d
kumackey <kumak1t09e0@gmail.com>
2024-03-19 11:01:46 +0900

    2.2まで

--- hello/src/main.rs
+++ hello/src/main.rs
@@ -1,4 +1,13 @@
+use std::process::Command;
+use std::env;
+
 fn main() {
-    println!("Hello, world!");
-    std::process::exit(1);
+    let args: Vec<String> = env::args().collect();
+
+    let output = Command::new("echo")
+        .args(&args[1..])
+        .output()
+        .expect("Failed to execute command");
+
+    println!("{}", String::from_utf8_lossy(&output.stdout));
 }
