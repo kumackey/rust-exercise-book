683ed4e24f1842c0231cc1a522d56b77da6cdafa
kumackey <kumak1t09e0@gmail.com>
2024-06-11 10:31:09 +0900

    途中まで

--- wcr/src/lib.rs
+++ wcr/src/lib.rs
@@ -15,6 +15,14 @@ pub struct Config {
     chars: bool,
 }
 
+#[derive(Debug, PartialEq)]
+pub struct FileInfo {
+    num_lines: usize,
+    num_words: usize,
+    num_bytes: usize,
+    num_chars: usize,
+}
+
 pub fn get_args() -> MyResult<Config> {
     let matches = App::new("wcr")
         .version("0.1.0")
@@ -32,7 +40,7 @@ pub fn get_args() -> MyResult<Config> {
                 .long("words")
                 .help("Show word count")
                 .takes_value(false),
-            )
+        )
         .arg(
             Arg::with_name("bytes")
                 .short("c")
@@ -46,8 +54,8 @@ pub fn get_args() -> MyResult<Config> {
                 .long("chars")
                 .help("Show character count")
                 .takes_value(false)
-                .conflict_with("bytes")
-            )
+                .conflicts_with("bytes")
+        )
         .arg(
             Arg::with_name("lines")
                 .short("l")
@@ -60,7 +68,7 @@ pub fn get_args() -> MyResult<Config> {
     let mut lines = matches.is_present("lines");
     let mut words = matches.is_present("words");
     let mut bytes = matches.is_present("bytes");
-    let chars = matches.is_present("chars")
+    let chars = matches.is_present("chars");
 
     if [lines, words, bytes, chars].iter().all(|&x| !x) {
         // 全てfalseだったら、デフォルトでline, word, byteをtrueにする
@@ -69,7 +77,7 @@ pub fn get_args() -> MyResult<Config> {
         bytes = true;
     }
 
-    Ok(Config{
+    Ok(Config {
         files: matches.values_of_lossy("files").unwrap(),
         lines: matches.is_present("lines"),
         words: matches.is_present("words"),
@@ -79,6 +87,84 @@ pub fn get_args() -> MyResult<Config> {
 }
 
 pub fn run(config: Config) -> MyResult<()> {
-    println!("{:?}", config);
+    for filename in &config.files {
+        match open(filename) {
+            Err(err) => eprintln!("{}: {}", filename, err),
+            Ok(_) => {
+                // 出力する
+                let info = count(open(filename)?);
+                match info {
+                    Err(err) => eprintln!("{}: {}", filename, err),
+                    Ok(info) => {
+                        if config.lines {
+                            println!("{} lines", info.num_lines);
+                        }
+                        if config.words {
+                            println!("{} words", info.num_words);
+                        }
+                        if config.bytes {
+                            println!("{} bytes", info.num_bytes);
+                        }
+                        if config.chars {
+                            println!("{} chars", info.num_chars);
+                        }
+                    }
+                }
+            }
+        }
+    }
+
     Ok(())
+}
+
+fn open(filename: &str) -> MyResult<Box<dyn BufRead>> {
+    match filename {
+        "-" => Ok(Box::new(BufReader::new(std::io::stdin()))),
+        _ => Ok(Box::new(BufReader::new(File::open(filename)?))),
+    }
+}
+
+pub fn count(mut file: impl BufRead) -> MyResult<FileInfo> {
+    let mut num_lines = 0;
+    let mut num_words = 0;
+    let mut num_bytes = 0;
+    let mut num_chars = 0;
+
+    let mut buf = String::new();
+    while file.read_line(&mut buf)? > 0 {
+        num_lines += 1;
+        num_bytes += buf.len();
+        num_chars += buf.chars().count();
+        num_words += buf.split_whitespace().count();
+        buf.clear();
+    }
+
+    Ok(FileInfo {
+        num_lines,
+        num_words,
+        num_bytes,
+        num_chars,
+    })
+}
+
+#[cfg(test)]
+mod tests {
+    use super::{count, FileInfo};
+    use std::io::Cursor;
+
+    #[test]
+    fn test_count() {
+        let data = "I don;t want the world, I just want your half.\r\n";
+        let info = count(Cursor::new(data));
+        assert!(info.is_ok());
+        assert_eq!(
+                info.unwrap(),
+                FileInfo {
+                    num_lines: 1,
+                    num_words: 12,
+                    num_bytes: 48,
+                    num_chars: 48,
+            },
+        );
+    }
 }
